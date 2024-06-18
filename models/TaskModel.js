55d2c4352e53161fd1cb88fc17e41504ee048bbd
sahil <sahilvichare81@gmail.com>
2024-06-18 16:04:38 +0530

    added certificate section

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const DiscussionSchema = new Schema({
+const DiscussionSchema = new Schema(
+  {
     question: { type: String },
     answer: { type: String },
-    username: { type: String }
-}, { timestamps: true });
+    username: { type: String },
+  },
+  { timestamps: true }
+);
 
-const TaskSchema = new Schema({
+const TaskSchema = new Schema(
+  {
     name: { type: String, required: true },
     taskId: { type: String, required: true, unique: true },
     companyId: { type: String, required: true },
-    companyLogo:{ type: String, required: true },
+    companyLogo: { type: String, required: true },
     companyName: { type: String, required: true },
     category: { type: String, required: true },
     taskReq: { type: String, required: true },
@@ -22,7 +26,9 @@ const TaskSchema = new Schema({
     helpLinks: { type: String },
     nice: { type: [String] },
     endDate: { type: Date, required: true },
-    discussions: { type: [DiscussionSchema] } // Embedded discussions array
-}, { timestamps: true });
+    discussions: { type: [DiscussionSchema] }, // Embedded discussions array
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Task', TaskSchema);
+module.exports = mongoose.model("Task", TaskSchema);
