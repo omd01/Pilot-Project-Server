99b3cdf06b27f63e53218c0b0fe7d9b1dac45c22
sahil <sahilvichare81@gmail.com>
2024-06-18 16:32:48 +0530

    updateed create certificate controller

--- controllers/certificateController.js
+++ controllers/certificateController.js
@@ -1,14 +1,58 @@
 // controllers/certificateController.js
 const Certificate = require('../models/CertificateModel');
+const CompanyProfile = require('../models/CompanyProfile');
+const Task = require("../models/TaskModel");
 
 // Create a new certificate
 exports.createCertificate = async (req, res) => {
   try {
-    const newCertificate = new Certificate(req.body);
+    const {
+      taskId,
+      taskName,
+      companyId,
+      companyName,
+      domain,
+      studentName,
+      studentNumber,
+      studentEmail,
+    } = req.body;
+
+   
+    // Fetch company logo from Company model
+    const company = await CompanyProfile.findOne({ companyId });
+    if (!company) {
+      
+      return res.status(404).json({ message: "Company not found" });
+    }
+  
+
+    // Fetch task description from Task model
+    const task = await Task.findOne({taskId});
+    if (!task) {
+     
+      return res.status(404).json({ message: "Task not found" });
+    }
+  
+    // Create a new certificate instance
+    const newCertificate = new Certificate({
+      taskId,
+      taskName,
+      companyName,
+      companyLogo: company.companyLogo,
+      desc: task.desc,
+      domain,
+      studentName,
+      studentEmail,
+    });
+
+    // Save the certificate to the database
     await newCertificate.save();
-    res.status(201).json(newCertificate);
+
+   
+    res.status(201).json({ message: "Certificate created successfully" });
   } catch (error) {
-    res.status(400).json({ message: 'Error creating certificate', error });
+    
+    res.status(500).json({ message: "Failed to create certificate", error });
   }
 };
 
